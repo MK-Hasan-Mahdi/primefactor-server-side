e494559173123734cf60ef9d28e3b2b28a50ad35
mk-hasan <mkhasan6891@gmail.com>
2022-05-06 00:51:29 +0600

    single inventory item loaded by id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
@@ -26,6 +26,13 @@ async function run() {
             const inventories = await cursor.toArray();
             res.send(inventories);
         });
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await inventoryCollection.findOne(query);
+            res.send(inventory);
+        })
     }
     finally {
 
