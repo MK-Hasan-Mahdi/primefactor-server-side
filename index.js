4781d33a165f9e6b61d5c05d6af799a83814fea3
mk-hasan <mkhasan6891@gmail.com>
2022-05-07 01:46:22 +0600

    delete method for delete inventory item per id

--- index.js
+++ index.js
@@ -20,26 +20,34 @@ async function run() {
         await client.connect();
         const inventoryCollection = client.db('primefactor').collection('inventory');
 
-        // get all inventory
+        // get all inventory item
         app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
             const inventories = await cursor.toArray();
             res.send(inventories);
         });
-        // get single item
+        // get single inventory item
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const inventory = await inventoryCollection.findOne(query);
             res.send(inventory);
         });
-        // post single item
+        // post single inventory item
         app.post('/inventory', async (req, res) => {
             const newItem = req.body;
             const output = await inventoryCollection.insertOne(newItem);
             res.send(output);
+        });
+        // delete inventory item 
+        app.delete('/inventory/:id', async (req, res) => {
+            const inventoryId = req.params.id;
+            const query = { _id: ObjectId(inventoryId) };
+            const output = await inventoryCollection.deleteOne(query);
+            res.send(output)
         })
+
     }
     finally {
 
