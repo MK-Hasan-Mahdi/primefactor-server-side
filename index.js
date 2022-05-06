801379892cc761cd7de7f274ea4d19232c917716
mk-hasan <mkhasan6891@gmail.com>
2022-05-06 21:15:15 +0600

    post method created for post single item in inventory

--- index.js
+++ index.js
@@ -20,18 +20,25 @@ async function run() {
         await client.connect();
         const inventoryCollection = client.db('primefactor').collection('inventory');
 
+        // get all inventory
         app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
             const inventories = await cursor.toArray();
             res.send(inventories);
         });
-
+        // get single item
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const inventory = await inventoryCollection.findOne(query);
             res.send(inventory);
+        });
+        // post single item
+        app.post('/inventory', async (req, res) => {
+            const newItem = req.body;
+            const output = await inventoryCollection.insertOne(newItem);
+            res.send(output);
         })
     }
     finally {
